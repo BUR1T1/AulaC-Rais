263fea75ef3aaf381101153b1ad64ae90661c57c
OTAVIO CAETANO RIBEIRO <otavio.ribeiro@cs.up.edu.br>
2025-03-26 19:19:25 -0300

    Primeira Aula

---
+++ controllers/PiveteControllers.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pivete.controlles 
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    class pivetecontrollers : ControllerBase
+    {
+        
+    }
+}
---
+++ models/pivete.cs
@@ -0,0 +1,12 @@
+namespace Novaula.models
+{
+    
+    public class pivete
+    {
+        public int Id{get; set;}
+        public string Nome{get; set;}
+        public string MaisTxt{get; set;}
+
+        
+    }
+}
